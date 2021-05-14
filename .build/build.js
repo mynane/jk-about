413fb1c8feb5361e32ac022b062936d7a8eacfca
”石进华“ <shijinhua@xiaoduotech.com>
2021-05-14 13:32:25 +0800

    0.1.4

--- .build/build.js
+++ .build/build.js
@@ -1,5 +1,6 @@
 const compressing = require("compressing");
-
+const fs = require("fs");
+fs.writeFileSync("./build/package.json", fs.readFileSync("./package.json"));
 compressing.zip
   .compressDir("./build", "build.zip")
   .then(() => {
